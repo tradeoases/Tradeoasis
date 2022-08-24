30ab7d76ad4ffeb9a8249a06826593e1caa49329
phillipmugisa <phillipmugisa4@gmail.com>
2022-08-25 01:08:50 +0300

    switching to tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/buyer/*html',
+    './templates/supplier/*html',
+    './templates/payments/*html',
+    './templates/manager/*html',
+  ],
+  theme: {
+    screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+    },
+    extend: {
+        colors: {
+            yellowColor: 'hsl(38, 95%, 47%)',
+            yellowColorHover: 'hsl(38, 95%, 44%)',
+            backgroundColor: 'hsl(0, 0%, 9%)',
+            lghterBackgroundColor: 'hsl(213, 28%, 19%)',
+            lighterBackgroundColorHover: 'hsl(213, 28%, 14%)',
+        },
+        width: {
+            'full': '95%',
+        },
+        gridTemplateRows: {
+            'auto-2': 'repeat(min-content, 2)',
+            'auto-3': 'repeat(min-content, 3)',
+        }
+    }
+},
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
