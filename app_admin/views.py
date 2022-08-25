d4d480cffb6fd49a5e56bdf36d6042db64f6a316
phillipmugisa <phillipmugisa4@gmail.com>
2022-08-26 00:40:14 +0300

    category upload imagefield to filefield

--- app_admin/views.py
+++ app_admin/views.py
@@ -346,10 +346,7 @@ class CategoryCreateView(SupportOnlyAccessMixin, View):
         sub_category_len = len(request.FILES) - 1
         for i in range(1, sub_category_len + 1):
             sub_cat_name = request.POST.get(f"subcategory-{i}")
-            sub_cat_image = request.FILES.get(f"subcategory-{i}")
-            print("*"*50)
-            print(sub_cat_image)
-            print("*"*50)
+            sub_cat_image = request.FILES.get(f"sub-category-image-{i}")
 
             if SupplierModels.ProductSubCategory.objects.filter(
                 name=string.capwords(sub_cat_name)
