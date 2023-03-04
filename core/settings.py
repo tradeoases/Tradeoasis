ff75a06cc915d84246a62289ffd9b303c84297e5
Phillip Mugisa <phillipmugisa4@gmail.com>
2023-03-04 21:09:00 +0300

    resuming

--- core/settings.py
+++ core/settings.py
@@ -192,10 +192,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-# STATIC_ROOT = 'static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+STATIC_ROOT = 'static'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
