eaeb6e865c03783ca7e955ddb19883410f89c7e4
Phillip Mugisa <phillipmugisa4@gmail.com>
2023-03-20 10:13:01 +0300

    pub

--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import _locale
+_locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
