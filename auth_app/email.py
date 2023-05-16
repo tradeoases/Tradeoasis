471c7fabdff03013c6773fe6d8e53a737164a2d0
Maximo Mugisha <maximugisha@gmail.com>
2023-05-16 11:54:11 +0000

    db.json

--- auth_app/email.py
+++ auth_app/email.py
@@ -5,9 +5,16 @@ from django.conf import settings
 
 
 def send_account_activation_email(name, email, subject, description):
-    context = {"name": name, "email": email, "description": description}
+    # context = {"name": name, "email": email, "description": description}
+    content = f"Hello {name},\n{description}.\nThank you."
     email_subject = subject
-    email_body = render_to_string("email_message.html", context)
+    email_body = render_to_string(
+        "email_message.html",
+        {
+            "content": "{}".format(content),
+            "image_url": None
+        },
+    )
 
     email = EmailMessage(
         email_subject,
