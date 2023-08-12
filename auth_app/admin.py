13cedcc0c315897e419b2892fec5a4d41479b9c3
Abdoulanziz <abdoulanzizally@outlook.com>
2023-08-12 14:38:08 +0300

    Initial commit.

--- auth_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.apps import apps
-
-# Register your models here.
-for model in apps.get_app_config("auth_app").get_models():
-    try:
-        admin.site.register(model)
-    except:
-        pass
--- supplier/templatetags/isEqual.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def isEqual(a, b):
-    return True if int(a) == int(b) else False
