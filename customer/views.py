3152bd9b5de6d99eaed3102513bcaa35397507a5
Abdoulanziz <abdoulanzizally@outlook.com>
2023-08-14 01:01:45 +0300

    Fix a few errors.

--- customer/views.py
+++ customer/views.py
@@ -6,36 +6,37 @@ from django.contrib.auth.decorators import login_required
 from .models import Article
 from .forms import ArticleForm
 from django.contrib.postgres.search import SearchVector
+# from django.http import HttpResponse
 
 
 # Create your views here.
 @login_required
 def key_metric_summary(request):
     # Get the ticket volume, average response time, resolution time, and customer satisfaction score.
-    ticket_volume = Ticket.objects.all().count()
-    average_response_time = Ticket.objects.all().aggregate(
-        Avg('response_time'))['response_time__avg']
-    resolution_time = Ticket.objects.all().aggregate(
-        Avg('resolution_time'))['resolution_time__avg']
-    customer_satisfaction_score = Ticket.objects.all().aggregate(
-        Avg('customer_satisfaction_score'))['customer_satisfaction_score__avg']
+    # ticket_volume = Ticket.objects.all().count()
+    # average_response_time = Ticket.objects.all().aggregate(
+    #     Avg('response_time'))['response_time__avg']
+    # resolution_time = Ticket.objects.all().aggregate(
+    #     Avg('resolution_time'))['resolution_time__avg']
+    # customer_satisfaction_score = Ticket.objects.all().aggregate(
+    #     Avg('customer_satisfaction_score'))['customer_satisfaction_score__avg']
 
-    # Get the last 3 lowest and highest 2 rated customer care representatives.
-    lowest_rated_representatives = Agent.objects.filter(
-        ratings__score__lt=customer_satisfaction_score).order_by('-ratings__score')[:3]
-    highest_rated_representatives = Agent.objects.filter(
-        ratings__score__gt=customer_satisfaction_score).order_by('ratings__score')[:2]
+    # # Get the last 3 lowest and highest 2 rated customer care representatives.
+    # lowest_rated_representatives = Agent.objects.filter(
+    #     ratings__score__lt=customer_satisfaction_score).order_by('-ratings__score')[:3]
+    # highest_rated_representatives = Agent.objects.filter(
+    #     ratings__score__gt=customer_satisfaction_score).order_by('ratings__score')[:2]
 
     # Render the template with the key metrics.
     context = {
-        'ticket_volume': ticket_volume,
-        'average_response_time': average_response_time,
-        'resolution_time': resolution_time,
-        'customer_satisfaction_score': customer_satisfaction_score,
-        'lowest_rated_representatives': lowest_rated_representatives,
-        'highest_rated_representatives': highest_rated_representatives,
+        # 'ticket_volume': ticket_volume,
+        # 'average_response_time': average_response_time,
+        # 'resolution_time': resolution_time,
+        # 'customer_satisfaction_score': customer_satisfaction_score,
+        # 'lowest_rated_representatives': lowest_rated_representatives,
+        # 'highest_rated_representatives': highest_rated_representatives,
     }
-    return render(request, 'key_metric_summary.html', context)
+    return render(request, 'customer/key_metric_summary.html', context)
 
 @login_required
 def rating_records(request, agent_id):
@@ -46,31 +47,31 @@ def rating_records(request, agent_id):
     context = {
         'rating_records': rating_records,
     }
-    return render(request, 'rating_records.html', context)
+    return render(request, 'customer/rating_records.html', context)
 
 @login_required
 def line_or_area_charts(request):
     # Get the ticket volume and average response time for the past 30 days.
-    ticket_volume_data = Ticket.objects.all().order_by('-created_at').values('created_at').annotate(
-        total_tickets=Count('id'))
-    response_time_data = Ticket.objects.all().order_by('-created_at').values('created_at').annotate(
-        average_response_time=Avg('response_time'))
+    # ticket_volume_data = Ticket.objects.all().order_by('-created_at').values('created_at').annotate(
+    #     total_tickets=Count('id'))
+    # response_time_data = Ticket.objects.all().order_by('-created_at').values('created_at').annotate(
+    #     average_response_time=Avg('response_time'))
 
-    # Prepare data for line charts
-    line_chart_1_data = []
-    for data in ticket_volume_data:
-        line_chart_1_data.append((data['created_at'], data['total_tickets']))
+    # # Prepare data for line charts
+    # line_chart_1_data = []
+    # for data in ticket_volume_data:
+    #     line_chart_1_data.append((data['created_at'], data['total_tickets']))
 
-    line_chart_2_data = []
-    for data in response_time_data:
-        line_chart_2_data.append((data['created_at'], data['average_response_time']))
+    # line_chart_2_data = []
+    # for data in response_time_data:
+    #     line_chart_2_data.append((data['created_at'], data['average_response_time']))
 
     # Render the template with the line chart data.
     context = {
-        'line_chart_1_data': line_chart_1_data,
-        'line_chart_2_data': line_chart_2_data,
+        # 'line_chart_1_data': line_chart_1_data,
+        # 'line_chart_2_data': line_chart_2_data,
     }
-    return render(request, 'line_or_area_charts.html', context)
+    return render(request, 'customer/line_or_area_charts.html', context)
 
 @login_required
 def gauges_or_kpi_cards(request):
@@ -89,7 +90,7 @@ def gauges_or_kpi_cards(request):
         'gauge_1_value': gauge_1_value,
         'gauge_2_value': gauge_2_value,
     }
-    return render(request, 'gauges_or_kpi_cards.html', context)
+    return render(request, 'customer/gauges_or_kpi_cards.html', context)
 
 @login_required
 def performance_comparison(request):
@@ -106,27 +107,27 @@ def performance_comparison(request):
     context = {
         'agent_data': agent_data,
     }
-    return render(request, 'performance_comparison.html', context)
+    return render(request, 'customer/performance_comparison.html', context)
 
 @login_required
 def response_time_analytics(request):
     # Calculate average response time for all tickets
-    average_response_time = Ticket.objects.all().aggregate(avg_response_time=Avg('response_time'))
+    # average_response_time = Ticket.objects.all().aggregate(avg_response_time=Avg('response_time'))
 
     context = {
-        'average_response_time': average_response_time['avg_response_time'],
+        # 'average_response_time': average_response_time['avg_response_time'],
     }
-    return render(request, 'response_time_analytics.html', context)
+    return render(request, 'customer/response_time_analytics.html', context)
 
 @login_required
 def resolution_time_analytics(request):
     # Calculate average resolution time for all tickets
-    average_resolution_time = Ticket.objects.all().aggregate(avg_resolution_time=Avg('resolution_time'))
+    # average_resolution_time = Ticket.objects.all().aggregate(avg_resolution_time=Avg('resolution_time'))
 
     context = {
-        'average_resolution_time': average_resolution_time['avg_resolution_time'],
+        # 'average_resolution_time': average_resolution_time['avg_resolution_time'],
     }
-    return render(request, 'resolution_time_analytics.html', context)
+    return render(request, 'customer/resolution_time_analytics.html', context)
 
 @login_required
 def customer_satisfaction_ratings(request):
@@ -136,7 +137,7 @@ def customer_satisfaction_ratings(request):
     context = {
         'average_customer_rating': average_customer_rating['avg_rating'],
     }
-    return render(request, 'customer_satisfaction_ratings.html', context)
+    return render(request, 'customer/customer_satisfaction_ratings.html', context)
 
 @login_required
 def agent_performance_reports(request):
@@ -149,7 +150,7 @@ def agent_performance_reports(request):
     context = {
         'agents': agents,
     }
-    return render(request, 'agent_performance_reports.html', context)
+    return render(request, 'customer/agent_performance_reports.html', context)
 
 # Article creation and management
 
@@ -168,7 +169,7 @@ def create_article(request):
     context = {
         'form': form,
     }
-    return render(request, 'create_article.html', context)
+    return render(request, 'customer/create_article.html', context)
 
 @login_required
 def edit_article(request, article_id):
@@ -185,7 +186,7 @@ def edit_article(request, article_id):
         'form': form,
         'article': article,
     }
-    return render(request, 'edit_article.html', context)
+    return render(request, 'customer/edit_article.html', context)
 
 @login_required
 def article_detail(request, article_id):
@@ -193,7 +194,7 @@ def article_detail(request, article_id):
     context = {
         'article': article,
     }
-    return render(request, 'article_detail.html', context)
+    return render(request, 'customer/article_detail.html', context)
 
 @login_required
 def search_articles(request):
@@ -204,7 +205,7 @@ def search_articles(request):
         'query': query,
         'articles': articles,
     }
-    return render(request, 'search_results.html', context)
+    return render(request, 'customer/search_results.html', context)
 
 @login_required
 def update_ticket_status(request, ticket_id):
@@ -213,7 +214,7 @@ def update_ticket_status(request, ticket_id):
     # (e.g., based on time elapsed or customer request).
     # You can use forms to allow authorized users to update the status.
 
-    return render(request, 'ticket_status_update.html', {'ticket': ticket})
+    return render(request, 'customer/ticket_status_update.html', {'ticket': ticket})
 
 @login_required
 def assign_ticket(request, ticket_id):
@@ -221,7 +222,7 @@ def assign_ticket(request, ticket_id):
     # Implement logic to assign the ticket to an agent.
     # You can use a dropdown form to select the agent for assignment.
 
-    return render(request, 'assign_ticket.html', {'ticket': ticket})
+    return render(request, 'customer/assign_ticket.html', {'ticket': ticket})
 
 @login_required
 def transfer_ticket(request, ticket_id):
@@ -229,4 +230,4 @@ def transfer_ticket(request, ticket_id):
     # Implement logic to transfer the ticket from one agent to another.
     # You can use a dropdown form to select the new agent for the transfer.
 
-    return render(request, 'transfer_ticket.html', {'ticket': ticket})
+    return render(request, 'customer/transfer_ticket.html', {'ticket': ticket})
